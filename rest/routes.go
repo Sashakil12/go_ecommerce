45bca1f45ad7f9ef3ee960efc5c3881765daeb26
Shakil <send2shakil@gmail.com>
2026-03-08 15:41:42 +0600

    + guard

--- rest/routes.go
+++ rest/routes.go
@@ -10,8 +10,8 @@ func InitRoutes(mux *http.ServeMux, manager *middleware.Manager) {
 	mux.Handle("GET /products", manager.With(http.HandlerFunc(handlers.GetProductsHandler)))
 	mux.Handle("POST /products", manager.With(http.HandlerFunc(handlers.CreateProductsHandler), middleware.Authentication))
 	mux.Handle("GET /products/{id}", manager.With(http.HandlerFunc(handlers.GetProductsByIdHandler)))
-	mux.Handle("PUT /products/{id}", manager.With(http.HandlerFunc(handlers.UpdateProductByIdHandler)))
-	mux.Handle("DELETE /products/{id}", manager.With(http.HandlerFunc(handlers.DeleteProductByIdHandler)))
+	mux.Handle("PUT /products/{id}", manager.With(http.HandlerFunc(handlers.UpdateProductByIdHandler), middleware.Authentication))
+	mux.Handle("DELETE /products/{id}", manager.With(http.HandlerFunc(handlers.DeleteProductByIdHandler), middleware.Authentication))
 	//users
 	mux.Handle("POST /users", manager.With(http.HandlerFunc(handlers.CreateUsersHandler)))
 	mux.Handle("POST /users/login", manager.With(http.HandlerFunc(handlers.LoginHandler)))
